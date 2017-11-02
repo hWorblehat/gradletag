6f30bceb7904dba213258ddfd987d547f0bd4ccc
hWorblehat <hWorblehat@users.noreply.github.com>
2017-11-02 10:40:25 +0000

    Properly check for grgit property on both current and root project

--- git/src/main/kotlin/org/uulib/gradletag/git/GitTagPlugin.kt
+++ git/src/main/kotlin/org/uulib/gradletag/git/GitTagPlugin.kt
@@ -5,6 +5,8 @@ import org.gradle.api.Project
 import org.uulib.gradletag.core.GradleTagPlugin
 import org.uulib.gradletag.core.GradleTagExtension
 import org.gradle.api.plugins.ExtensionAware
+import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
 
 class GitTagPlugin : Plugin<Project> {
 	
@@ -13,9 +15,21 @@ class GitTagPlugin : Plugin<Project> {
 		
 		val gradletag = project.extensions.getByType(GradleTagExtension::class.java)
 		
-		project.pluginManager.withPlugin("org.ajoberstar.grgit", { _ ->
-			gradletag.vcs.set(project.property("grgit"))
-		})
+		val rootGrgit = project.objects.property(Any::class.java)
+		val grgit = project.objects.property(Any::class.java)
+		grgit.setProvider(rootGrgit)
+		
+		project.rootProject.pluginManager.withPlugin("org.ajoberstar.grgit") { _ ->
+			rootGrgit.set(project.rootProject.property("grgit"))
+			gradletag.vcs.setProvider(grgit)
+		}
+		
+		project.pluginManager.withPlugin("org.ajoberstar.grgit") { _ ->
+			grgit.set(project.property("grgit"))
+			gradletag.vcs.setProvider(grgit)
+		}
 	}
 	
 }
+
+private fun <T> Property<T>.setProvider(provider: Provider<out T>) = set(provider)
